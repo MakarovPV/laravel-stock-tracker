f3e28d71aa06bed92ffbbd31828246c8f9b27800
MakarovPV <halarden1@gmail.com>
2023-05-24 16:23:21 +0500

    added cache

--- app/Http/Controllers/Api/StockApiController.php
+++ app/Http/Controllers/Api/StockApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Modules\StartDateSetting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class StockApiController extends Controller
@@ -16,39 +17,63 @@ class StockApiController extends Controller
     public function moscow(Request $request)
     {
         $data = $request->query();
+        $cacheKey = $data['ticker'] . '_' . $data['interval'];
         $date = (new StartDateSetting($data['segment']))->selectInterval();
 
-        return Http::get("https://iss.moex.com/iss/engines/stock/markets/shares/securities/{$data['ticker']}/candles.json", [
-            'iss.meta' => 'off',
-            'interval' => $data['interval'],
-            'from' => $date,
-        ])->throw()->json('candles.data');
+        $result = Cache::get($cacheKey, false);
+
+        if(!$result){
+            $result = Http::get("https://iss.moex.com/iss/engines/stock/markets/shares/securities/{$data['ticker']}/candles.json", [
+                'iss.meta' => 'off',
+                'interval' => $data['interval'],
+                'from' => $date,
+            ])->throw()->json('candles.data');
+
+            Cache::put($cacheKey, $result, 3600);
+        }
+        return $result;
     }
 
     //Получение данных по API иностранной биржи.
     public function foreign(Request $request)
     {
         $data = $request->query();
+        $cacheKey = $data['ticker'] . '_' . $data['interval'];
 
-        return Http::get("https://www.alphavantage.co/query", [
-            'function' => "time_series_{$data['segment']}",
-            'symbol' => $data['ticker'],
-            'market' => 'usd',
-            'interval' => $data['interval'] . 'min',
-            'apikey' => $this->apikey,
-        ])->throw()->json();
+        $result = Cache::get($cacheKey, false);
+
+        if(!$result){
+            $result = Http::get("https://www.alphavantage.co/query", [
+                'function' => "time_series_{$data['segment']}",
+                'symbol' => $data['ticker'],
+                'market' => 'usd',
+                'interval' => $data['interval'] . 'min',
+                'apikey' => $this->apikey,
+            ])->throw()->json();
+
+            Cache::put($cacheKey, $result, 3600);
+        }
+        return $result;
     }
 
     //Получение данных по API криптовалюты.
     public function cryptocurrency(Request $request)
     {
         $data = $request->query();
+        $cacheKey = $data['ticker'] . '_' . $data['interval'];
 
-        return Http::get("https://min-api.cryptocompare.com/data/v2/{$data['segment']}", [
-            'fsym' => $data['ticker'],
-            'tsym' => 'rub',
-            'aggregate' => $data['interval'],
-            'limit' => $data['limit'],
-        ])->throw()->json();
+        $result = Cache::get($cacheKey, false);
+
+        if(!$result){
+            $result = Http::get("https://min-api.cryptocompare.com/data/v2/{$data['segment']}", [
+                'fsym' => $data['ticker'],
+                'tsym' => 'rub',
+                'aggregate' => $data['interval'],
+                'limit' => $data['limit'],
+            ])->throw()->json();
+
+            Cache::put($cacheKey, $result, 3600);
+        }
+        return $result;
     }
 }
